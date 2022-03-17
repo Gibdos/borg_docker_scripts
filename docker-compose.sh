28f052aef4da431d1ec93a054ca813f3254ba741
gibdos <35290287+Gibdos@users.noreply.github.com>
2022-03-17 19:19:25 +0100

    Delete docker-compose.sh

--- docker-compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# specify the default repository to use for backups
-export BORG_REPO='ssh://HOST:PORT/./docker-compose'
-
-# Run Borg backup
-/usr/bin/borg create --stats ::$(hostname)-$(date -I) /opt/
-
-# Prune old backups
-/usr/bin/borg prune -v --list --keep-daily 7 --keep-weekly 1
